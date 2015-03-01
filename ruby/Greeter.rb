029df72ac78e02338072473432156977a46c9ba6
Joshua Eliser <jeliser@gmail.com>
2015-03-01 22:25:05 +0000

    : #comment Added what amounts to a driver class to the Greeter class.  Pretty cool way to have a main() in a class and have a method to run through a set of tests that pertain directly to the class being defined.

--- ruby/Greeter.rb
+++ ruby/Greeter.rb
@@ -37,4 +37,33 @@ class Greeter
 end
 
 
+# Pretty cool way having a driver test for the class
+if __FILE__ == $0
+  me = Greeter.new("Josh")
+  me.say_hi
+  me.say_bye
 
+  # Looks like local and instance variables are both valid methods to save instances.
+
+  #puts Greeter.instance_methods
+  puts Greeter.instance_methods(false)
+
+  puts me.respond_to?("say_hi")
+  puts me.respond_to?("to_s")
+
+  # Allow name to be accessable
+  puts me.respond_to?("names")
+  puts me.respond_to?("names=")
+  class Greeter
+    # Crazy ass runtime modification of a classes' instance variables.
+    attr_accessor :names
+  end
+  puts me.respond_to?("names")
+  puts me.respond_to?("names=")
+
+  me.say_hi
+  me.names = ["Monk", "Gidget", "Tucker"]
+  me.say_hi
+  me.say_bye
+
+end

c422e814d5bc8c1ff830031cd548f301ab8b0eb0
Joshua Eliser <jeliser@gmail.com>
2018-03-28 09:59:57 -0600

    Added a periodic writer application to demonstrate the problem fstream when file operations are happening.

---
+++ cpp/src/periodic_file_writer.cpp
@@ -0,0 +1,28 @@
+// basic file operations
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include <signal.h>
+
+volatile sig_atomic_t done = 0;
+void signalHandler(int sig){ // can be called asynchronously
+  done = 1; // set flag
+}
+
+
+int main () {
+  signal(SIGINT, signalHandler);
+
+  std::ofstream myfile;
+  myfile.open ("/tmp/example.log");
+
+  for( ; !done; ) {
+    std::this_thread::sleep_for (std::chrono::seconds(1));  
+    myfile << "Writing this to a file.\n";
+  }
+
+  myfile.close();
+
+  return 0;
+}

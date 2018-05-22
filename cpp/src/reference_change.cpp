455f0df50bd1deb3d952989fe4043df3165cf79f
Joshua Eliser <jeliser@gmail.com>
2018-05-22 15:16:40 -0600

    Learned about std::reference_wrapper.

---
+++ cpp/src/reference_change.cpp
@@ -0,0 +1,25 @@
+// https://stackoverflow.com/questions/7713266/how-can-i-change-the-variable-to-which-a-c-reference-refers
+
+#include <string>
+#include <iostream>
+#include <functional>
+
+int main() {
+
+  uint32_t valA = 123;
+  uint32_t valB = 987;
+  auto ref = std::ref(valA);
+
+  //  123  987  123
+  std::cout << valA << "  " << valB << "  " << ref << std::endl;
+
+  //  123  987  987
+  ref = std::ref(valB);
+  std::cout << valA << "  " << valB << "  " << ref << std::endl;
+
+  //  123  456  456
+  ref.get() = 456;
+  std::cout << valA << "  " << valB << "  " << ref << std::endl;
+
+}
+

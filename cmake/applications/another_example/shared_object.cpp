32f15e3a87a21c0428a3075d169cf2de7b5881f3
Joshua Eliser <joshua.eliser@kelvininc.com>
2018-10-01 09:31:31 -0600

    Made some poor coding formatting so that I can demo cleaning it up.

--- cmake/applications/another_example/shared_object.cpp
+++ cmake/applications/another_example/shared_object.cpp
@@ -1,22 +1,34 @@
 #include "shared_object.h"
 #include <experimental/filesystem>
 
-int hello_world() {
+int hello_world()
+{
   return 12345;
 }
 
-void HelloWorld::print() {
+void HelloWorld::print()
+{
   // Check that the build system is actually using c++17
-  auto exists = std::experimental::filesystem::exists("/some/made/up/directory");
-  printf("How are you?  %s", exists ? "true" : "false");
+  auto exists =
+    std::experimental::filesystem::exists("/some/made/up/directory");
+
+      printf("How are you?  %s", exists ? "true" : "false");
 }
 
-std::list<std::string> HelloWorld::get_list() {
+std::list<std::string>
+HelloWorld::get_list()
+{
   return std::list<std::string>({"hello", "world", "this", "is", "a", "list"});
 }
 
 // This function is forcing all the methods in the Interface to be implemented.
-std::unique_ptr<HelloWorldInterface> newInstance() {
+std::unique_ptr<HelloWorldInterface>
+
+newInstance() {
   return std::make_unique<HelloWorld>();
 }
+
+
+
+
 

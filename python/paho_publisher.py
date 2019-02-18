ef20b25508bebf753d7ae2c8ef53832ba3b7ac35
Joshua Eliser <joshua.eliser@kelvininc.com>
2019-02-18 12:17:18 -0700

    Some example Sparkplug data and a replay mechanism for testing out the libraries.

--- python/paho_publisher.py
+++ python/paho_publisher.py
@@ -2,51 +2,36 @@
 import paho.mqtt.client as mqttClient
 import time
 import struct
- 
+
 def on_connect(client, userdata, flags, rc):
- 
     if rc == 0:
- 
         print("Connected to broker")
- 
         global Connected                #Use global variable
         Connected = True                #Signal connection 
- 
     else:
- 
         print("Connection failed")
- 
-def on_message(client, userdata, message):
-    print "Message received: "  + message.payload
-    with open('sparkplug_data.raw', 'ab') as f:
-        f.write(struct.pack('I', len(message.payload)))
-        f.write(message.payload)
                     
- 
 Connected = False   #global variable for the state of the connection
  
 broker_address= "127.0.0.1"  #Broker address
 port = 1883                         #Broker port
-user = "yourUser"                    #Connection username
-password = "yourPassword"            #Connection password
  
-client = mqttClient.Client("Python")               #create new instance
-#client.username_pw_set(user, password=password)    #set username and password
+client = mqttClient.Client("PythonPub")            #create new instance
 client.on_connect= on_connect                      #attach function to callback
-client.on_message= on_message                      #attach function to callback
- 
 client.connect(broker_address, port=port)          #connect to broker
- 
 client.loop_start()        #start the loop
  
 while Connected != True:    #Wait for connection
     time.sleep(0.1)
-## The '#' will subscribe to all topics on the bus
-client.subscribe("#")
  
 try:
-    while True:
-        time.sleep(1)
+    with open('sparkplug_data.raw', 'rb') as f:
+        raw = 1
+        while raw:
+            payload_len = struct.unpack('I', f.read(4))[0]
+            raw = struct.unpack('%ds' % (payload_len), f.read(payload_len))[0]
+            client.publish('replay', raw)
+            time.sleep(1)
  
 except KeyboardInterrupt:
     print "exiting"

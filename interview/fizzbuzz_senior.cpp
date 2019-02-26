d0d0f7ee6769752ced760cbc7e917cd0e8ee3def
Joshua Eliser <joshua.eliser@kelvininc.com>
2019-02-26 09:02:41 -0700

    Updated the fizzbuzz example to include a senior leave coding challenge.

---
+++ interview/fizzbuzz_senior.cpp
@@ -0,0 +1,23 @@
+// Compiled with: g++ -Wall -std=c++14 -pthread
+
+#include <iostream>
+
+////////////////////////////////////////
+// Write a template function that returns a string based on an input value and include
+// a template specialization for the string templated type.  Other types will use the generalized template.
+//   - When the input is a multiple of 3 output 'fizz'
+//   - When the input is a multiple of 5 output 'buzz'
+//   - When the input is a multiple of 3 and 5 output 'fizzbuzz'
+//   - Otherwise output the number as a string.
+////////////////////////////////////////
+
+
+
+
+int main() {
+    
+    // Print out the results from [0, 20) for template types: uint16_t, int32_t, string, and float.
+    
+    return 0;
+}
+

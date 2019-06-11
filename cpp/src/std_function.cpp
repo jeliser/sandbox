0a9d6bfa71744add1fc6204b1f54a7438fe0fc15
Joshua Eliser <joshua.eliser@kelvininc.com>
2019-06-11 10:14:20 -0600

    Added additional examples to the std::function example application.

--- cpp/src/std_function.cpp
+++ cpp/src/std_function.cpp
@@ -3,14 +3,42 @@
 #include <string>
 #include <iostream>
 #include <functional>
+#include <map>
+
+void test(bool val)
+{
+  printf("%d\n", val);
+}
+
+typedef struct {
+  std::function<void (bool)> callback;
+} CallbackProperties;
 
 int main() {
 
   auto func1 = std::function<void(bool)>();
 
-  if(func1)
+  if(func1) {
     std::cout << "We're a function" << std::endl;
-  else
+  } else {
     std::cout << "No function" << std::endl;
+  }
+
+  func1 = std::bind(test, std::placeholders::_1);
+  if(func1) {
+    std::cout << "We're a function" << std::endl;
+    func1(false);
+    func1(true);
+  } else {
+    std::cout << "No function" << std::endl;
+  }
+
+  func1 = nullptr;
+
+  std::map<int, CallbackProperties> callbacks;
+  callbacks.emplace(0, CallbackProperties{std::bind(test, std::placeholders::_1)});
+  auto found = callbacks.find(0);
+  found->second.callback(true);
+  callbacks.clear();
 }
 

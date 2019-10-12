a76b951a6b9fe76431af022ee94f8f81906301aa
Joshua Eliser <joshua.eliser@kelvininc.com>
2019-10-12 08:51:22 -0600

    Added support for the mutlimethod library that allows for overloaded methods in python.

--- python/class_callbacks.py
+++ python/class_callbacks.py
@@ -3,13 +3,16 @@
 import sys
 import os
 import functools
+from multimethod import overload, isa, multimeta
 
 from MessageInterface import MessageInterface
 
 from data_model.raw.double import Double as Double
 from data_model.duplicate.namespace.double import Double as DDouble
-#import data_model.raw.double.Double
 
+
+###################################
+# Simple inline data model ... the other types are imported
 
 class Color(MessageInterface):
     def __init__(self, topic = ""):
@@ -34,28 +37,81 @@ class Float(MessageInterface):
     def set_double(self, double):
         self.double = double
 
-# Takes a string and 
+
+# Takes a string and looks for a matching class in the module space
 def str_to_class(class_name):
     try:
         return functools.reduce(getattr, class_name.split("."), sys.modules[__name__])
     except:
         return None
 
-# Show that we can take a class name as a string and find a matching class implementation
-print(str_to_class("MessageInterface"))
-print(str_to_class("Color"))
-print(str_to_class("Float"))
-print(str_to_class("Double"))
-print(str_to_class("DDouble"))
-print(str_to_class("data_model.raw.double.Double"))
-print(str_to_class("fake_class"))
-print(DDouble)
+###################################
 
-# Create instances of the class
+# Show that we can take a class name as a string and find a matching class implementation
+class_names = ["MessageInterface", "Color", "Float", "Double", "DDouble"]
+
+lookup = dict()
+for class_name in class_names:
+    lookup[class_name] = str_to_class(class_name)
+
+if not all(lookup.values()):
+    print('Failed to lookup a class typen:\n{}'.format(lookup));
+
+# Create instances of the classes that we can use in the callbacks
 m = MessageInterface()
 c = Color("red")
 f = Float("my_value")
-dd = DDouble("my_double")
+d = Double("my_double")
+dd = DDouble("my_duplicate_double")
+
+###################################
+## Example of using overloads outside of a class definition
+
+## All the overloaded functions
+@overload
+def callback(data: isa(MessageInterface)):
+    print('{:16} -- {}'.format('MessageInterface', data))
+
+@overload
+def callback(data: isa(Float)):
+    print('{:16} -- {}'.format('Float', data))
+
+@overload
+def callback(data: isa(Color)):
+    print('{:16} -- {}'.format('Color', data))
+
+# This will trigger the specific callbacks
+print('Non-class methods:')
+callback(f)
+callback(c)
+# This will trigger the catch-all callback
+callback(d)
+callback(dd)
+print('')
+
+###################################
+## Example of using overloads inside of a class definition
+
+class DataScienceApplication(metaclass=multimeta):
+    def on_data(self, data: Float):
+        print('{:16} -- {}'.format('Float', data))
+
+    def on_data(self, data: Double):
+        print('{:16} -- {}'.format('Double', data))
+
+    def on_data(self, data: DDouble):
+        print('{:16} -- {}'.format('DDouble', data))
+
+    def on_data(self, data: MessageInterface):
+        print('{:16} -- {}'.format('MessageInterface', data))
 
 
+app = DataScienceApplication()
+
+
+print('Class methods:')
+app.on_data(f)
+app.on_data(c)
+app.on_data(d)
+app.on_data(dd)
 

cd710035d67fa20f3fdb9f2dc677c27e7310463e
Joshua Eliser <jeliser@gmail.com>
2020-05-09 18:26:04 -0600

    master: Added additional options to the topic listing and cleaned up the class.

--- ros/motion_controller.py
+++ ros/motion_controller.py
@@ -6,64 +6,96 @@ import math
 import rospy
 import traceback
 
-from std_msgs.msg import Float64
+from enum import Enum
+
+from std_msgs.msg import Float64, Int32, String
 from geometry_msgs.msg import Vector3
 
+## This will all get read from a configuration file
 MENU = 'Use the arrow keys to drive: Up/Down = Throttle, Left/Right = Steering\n'
 ROBOT = 'scout_1'
 SUBSYSTEM = 'wwr/motion_controller'
 
-class ScoutMotorControl():
+WHEEL_CONTROLLER = 'wheel_controller'
+STEERING_ARM_CONTROLLER = 'steering_arm_controller'
+
+FR = 'fr'
+FL = 'fl'
+BR = 'br'
+BL = 'bl'
+
+class State(Enum):
+    UNKNOWN = 0
+    INVALID = 0
+    INACTIVE = 1
+    BUSY = 2
+    STATIONARY = 3
+    ACTIVE_BRAKING = 4
+    IN_MOTION = 5
+
+class DriveMode(Enum):
+    UNKNOWN = 0
+    ACKERMANN = 1
+    SKID_STEER = 2
+
+
+class BaseRoverMotionController():
 
     def __init__(self, robot):
-        self.FR_WHEEL_TOPIC = '/{}/fr_wheel_controller/command'.format(ROBOT)
-        self.FL_WHEEL_TOPIC = '/{}/fl_wheel_controller/command'.format(ROBOT)
-        self.BR_WHEEL_TOPIC = '/{}/br_wheel_controller/command'.format(ROBOT)
-        self.BL_WHEEL_TOPIC = '/{}/bl_wheel_controller/command'.format(ROBOT)
-
-        self.FR_STEER_TOPIC = '/{}/fr_steering_arm_controller/command'.format(ROBOT)
-        self.FL_STEER_TOPIC = '/{}/fl_steering_arm_controller/command'.format(ROBOT)
-        self.BR_STEER_TOPIC = '/{}/br_steering_arm_controller/command'.format(ROBOT)
-        self.BL_STEER_TOPIC = '/{}/bl_steering_arm_controller/command'.format(ROBOT)
-
         rospy.init_node('motor_controller', anonymous=True)
-        self.fr_wheel = rospy.Publisher(self.FR_WHEEL_TOPIC, Float64, queue_size=10)
-        self.fl_wheel = rospy.Publisher(self.FL_WHEEL_TOPIC, Float64, queue_size=10)
-        self.br_wheel = rospy.Publisher(self.BR_WHEEL_TOPIC, Float64, queue_size=10)
-        self.bl_wheel = rospy.Publisher(self.BL_WHEEL_TOPIC, Float64, queue_size=10)
 
-        self.fr_steer = rospy.Publisher(self.FR_STEER_TOPIC, Float64, queue_size=10)
-        self.fl_steer = rospy.Publisher(self.FL_STEER_TOPIC, Float64, queue_size=10)
-        self.br_steer = rospy.Publisher(self.BR_STEER_TOPIC, Float64, queue_size=10)
-        self.bl_steer = rospy.Publisher(self.BL_STEER_TOPIC, Float64, queue_size=10)
+        # Initialize the controller
+        self.state = State.UNKNOWN
+        self.drive_mode = DriveMode.UNKNOWN
+        self.wheels = dict()
+        self.steer_arms  = dict()
+        for _id in [FR, FL, BR, BL]:
+            self.wheels[_id] = rospy.Publisher('/{}/{}_{}/command'.format(robot, _id, WHEEL_CONTROLLER), Float64, queue_size=1)
+            self.steer_arms[_id] = rospy.Publisher('/{}/{}_{}/command'.format(robot, _id, STEERING_ARM_CONTROLLER), Float64, queue_size=1)
 
     def set_command(self, data):
         throttle = data.x
         steer    = data.y
 
-        self.fr_wheel.publish(throttle)
-        self.fl_wheel.publish(throttle)
-        self.br_wheel.publish(throttle)
-        self.bl_wheel.publish(throttle)
+        # Publish the wheel throttle and steering data
+        for _, v in self.wheels.items():
+            v.publish(throttle)
+        for k, v in self.steer_arms.items():
+            v.publish(steer if 'f' in k else -steer)
 
-        self.fr_steer.publish(-steer)
-        self.fl_steer.publish(-steer)
-        self.br_steer.publish(steer)
-        self.bl_steer.publish(steer)
+    def get_state(self):
+        return self.state
 
+    def set_drive_mode(self, mode):
+        for e in DriveMode:
+            if mode.data == e.name:
+                self.drive_mode = e
+                return
+        print('Failed to set drive mode to {} -- {}'.format(mode, [e.name for e in DriveMode]))
+
+    def get_drive_mode(self):
+        return self.drive_mode.name
 
 class MotionSubsystem():
 
     def __init__(self, robot):
-        self.motion_system = ScoutMotorControl(robot)
+        self.motion_controller = BaseRoverMotionController(robot)
+
+        self.motion_command = rospy.Subscriber("/{}/{}/motion/command".format(robot, SUBSYSTEM), Vector3, self.motion_controller.set_command);
+        self.drive_mode_command = rospy.Subscriber("/{}/{}/drive_mode/command".format(robot, SUBSYSTEM), String, self.motion_controller.set_drive_mode);
+        self.drive_mode = rospy.Publisher("/{}/{}/drive_mode".format(robot, SUBSYSTEM), String, queue_size=1)
 
     def run(self):
-        self.motion_command = rospy.Subscriber("/{}/{}/motion_command".format(ROBOT, SUBSYSTEM), Vector3, self.motion_system.set_command);
-        rospy.spin()
+        rate = rospy.Rate(0.1) # 1Hz
+        while not rospy.is_shutdown():
+            self.drive_mode.publish(self.motion_controller.get_drive_mode())
+            rate.sleep()
 
 if __name__ == '__main__':
     try:
         control = MotionSubsystem(ROBOT)
         control.run()
     except rospy.ROSInterruptException:
+        pass
+    except:
         traceback.print_exc()

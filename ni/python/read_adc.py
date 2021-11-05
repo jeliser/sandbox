49a7420a9ef6100b13ad51c4f61ad57d727b1cf8
Joshua Eliser <jeliser@gmail.com>
2021-11-05 16:09:10 -0500

    Finally figured out how to read the device AI pin.  The device name was throwing me off when I originally was working this and it makes a bunch more sense.

--- ni/python/read_adc.py
+++ ni/python/read_adc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import nidaqmx
+import time
 import argparse
 import traceback
 
@@ -7,10 +8,17 @@ import traceback
 parser = argparse.ArgumentParser(description='Publishes rows of data.')
 args = parser.parse_args()
 
+help(nidaqmx.system)
+print(nidaqmx.system.device)
+
 try:
   with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan('Dev1/ai0')
-    task.read()
+    task.ai_channels.add_ai_voltage_chan('Dev2/ai0')
+    for i in range(0,10):
+      print(task.read())
+      time.sleep(1)
+except KeyboardInterrupt:
+  pass
 except:
     traceback.print_exc()
 

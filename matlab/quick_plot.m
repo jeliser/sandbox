b74aea352a8786a29fe99a6f430288a7979f1893
Joshua Eliser <jeliser@gmail.com>
2021-11-23 13:03:42 -0600

    Added a quick operation to determine the focus of the image.

--- matlab/quick_plot.m
+++ matlab/quick_plot.m
@@ -50,4 +50,49 @@ function [arr, timestamps, data]= quick_plot(filename)
 %     plot(data.PELTIER01,"Color",'blue','LineWidth',2.0);
 %     plot(data.PELTIER02,"Color",'cyan','LineWidth',2.0);
 %     legend('HEATER01', 'PELTIER01', 'PELTIER02');
+
+%% Image plotting
+close all;
+figure();
+
+images = {
+    'C:\Users\Joshua Eliser\Desktop\code\sandbox\matlab\img_focus_none.tif';
+    'C:\Users\Joshua Eliser\Desktop\code\sandbox\matlab\img_focus_some.tif';
+    'C:\Users\Joshua Eliser\Desktop\code\sandbox\matlab\img_focus_mostly.tif'
+    };
+
+num_images = numel(images);
+sums = [];
+
+for i = 1:num_images
+    img = imread(images{i});
+    log_shifted = log(abs(fftshift(fft2(double(img)))));
+    sums = [sums; sum(log_shifted, 1)];
+
+    subplot(num_images, 2, ((i-1)*2)+1);
+    imshow(log_shifted, []);
     
+    subplot(num_images, 2, ((i-1)*2)+2);
+    plot(sums(i, :));
+end
+
+% 
+figure();
+
+% Reduce the range of samples to the ones around the spike.
+range_lower = 1500;
+range_upper = 2499;
+
+subplot(2, 1, 1);
+hold on;
+plot(sums(1, range_lower:range_upper));
+plot(sums(2, range_lower:range_upper));
+plot(sums(3, range_lower:range_upper));
+
+subplot(2, 1, 2);
+sosums = [];
+for sosum = 1 : size(sums, 1)
+    sosums = [sosums sum(sums(i, range_lower:range_upper))];
+end
+bar(std(sums'));
+

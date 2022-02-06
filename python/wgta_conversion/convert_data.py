9a5c0aed712117051ed549ed9dfe5ef5ca7a0c50
Joshua Eliser <jeliser@gmail.com>
2022-02-05 21:45:34 -0600

    master: Able to process the while directory of files.

--- python/wgta_conversion/convert_data.py
+++ python/wgta_conversion/convert_data.py
@@ -16,8 +16,8 @@ def main():
   parser.add_argument(      '--verbose-help', help='Prints out the verbose help menu', action='store_true', default=False)
   args = parser.parse_args()
 
-  args.input_dir = os.path.abspath(args.input_dir)
-  args.output_dir = os.path.abspath(args.output_dir)
+  args.input_dir = os.path.join(os.path.abspath(args.input_dir), '')
+  args.output_dir = os.path.join(os.path.abspath(args.output_dir), '')
 
   os.makedirs(args.output_dir, exist_ok=True)
 
@@ -27,19 +27,21 @@ def main():
     sys.exit()
 
   try:
-    for name in glob.glob(f'{args.input_dir}/*/*.csv'):
+    for name in glob.glob(f'{args.input_dir}/**/*.csv', recursive=True):
       # Read the data into a pandas data frame and remove all the duplicate lines to only leave the latest updated row.
       df=pd.read_csv(name, sep=',')
       ## where and roll are apparently the key
       ## https://stackoverflow.com/questions/19125661/find-index-where-elements-change-value-numpy
       keys = list(filter(lambda k: 'SUBSEC' in k, df.columns))
-      print(keys)
       if any(keys):
+        if len(keys) ~= 1:
+          print('There are multiple keys that match in the file.')
         print(f'Filtering on {keys[0]} in {name.split(args.input_dir)[-1]}')
         v = df[keys[0]]  # TODO: This should verify that it's only a single key been found
         idx = np.where(np.roll(v,-1)!=v)[0]
         fdf = df.iloc[idx]
-        fdf.to_csv(os.path.join(args.output_dir, os.path.basename(name)), index=False)
+        os.makedirs(os.path.split(os.path.join(args.output_dir, name.split(args.input_dir)[-1]))[0], exist_ok=True)
+        fdf.to_csv(os.path.join(args.output_dir, name.split(args.input_dir)[-1]), index=False)
     pass
   except KeyboardInterrupt: 
     running = False

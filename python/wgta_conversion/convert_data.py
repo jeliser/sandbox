0474e8c4d4f555c81f8851a95fa95aa503ba787d
Joshua Eliser <jeliser@gmail.com>
2022-02-05 22:55:09 -0600

    master: Processed all the scenarios and placed them in a simplified folder structured.

--- python/wgta_conversion/convert_data.py
+++ python/wgta_conversion/convert_data.py
@@ -13,6 +13,7 @@ def main():
   parser = argparse.ArgumentParser(description='Publishes rows of data.')
   parser.add_argument('-i', '--input-dir', help='The directory with the raw CSV files to be processed', required=True)
   parser.add_argument('-o', '--output-dir', help='The directory to place the converted CSV data', required=True)
+  parser.add_argument('-v', '--verbose', help='Prints out the verbose processing information', action='store_true', default=False)
   parser.add_argument(      '--verbose-help', help='Prints out the verbose help menu', action='store_true', default=False)
   args = parser.parse_args()
 
@@ -28,20 +29,29 @@ def main():
 
   try:
     for name in glob.glob(f'{args.input_dir}/**/*.csv', recursive=True):
+      ignore = ['LTF', 'MEG', 'Purge']
+      if any(filter(lambda i: i in name, ignore)):
+        continue
       # Read the data into a pandas data frame and remove all the duplicate lines to only leave the latest updated row.
-      df=pd.read_csv(name, sep=',')
-      ## where and roll are apparently the key
-      ## https://stackoverflow.com/questions/19125661/find-index-where-elements-change-value-numpy
-      keys = list(filter(lambda k: 'SUBSEC' in k, df.columns))
-      if any(keys):
-        if len(keys) ~= 1:
-          print('There are multiple keys that match in the file.')
-        print(f'Filtering on {keys[0]} in {name.split(args.input_dir)[-1]}')
-        v = df[keys[0]]  # TODO: This should verify that it's only a single key been found
-        idx = np.where(np.roll(v,-1)!=v)[0]
-        fdf = df.iloc[idx]
-        os.makedirs(os.path.split(os.path.join(args.output_dir, name.split(args.input_dir)[-1]))[0], exist_ok=True)
-        fdf.to_csv(os.path.join(args.output_dir, name.split(args.input_dir)[-1]), index=False)
+      df=pd.read_csv(name, sep=',', low_memory=False)
+      if('Systems.csv' in name or 'GNCA.csv' in name):
+        opath = os.path.join(args.output_dir, os.path.basename(name).split('_')[0])
+        print(f'Processing: {name.split(args.input_dir)[-1]} to {opath}')
+        
+        # We are going to filter on any SUBSEC column in the CSV.  That's where the data will be aligned for the current timestep
+        keys = list(filter(lambda k: 'SUBSEC' in k, df.columns))
+        if any(keys):
+          if args.verbose:
+            print(f'Filtering on {keys[0]} in {name.split(args.input_dir)[-1]}')
+
+          ## np.where and np.roll are apparently the key
+          ## https://stackoverflow.com/questions/19125661/find-index-where-elements-change-value-numpy
+          v = df[keys[0]]  # TODO: This should verify that it's only a single key been found
+          idx = np.where(np.roll(v,-1)!=v)[0]
+          fdf = df.iloc[idx]
+
+          os.makedirs(opath, exist_ok=True)
+          fdf.to_csv(os.path.join(opath, os.path.basename(name)), index=False)
     pass
   except KeyboardInterrupt: 
     running = False

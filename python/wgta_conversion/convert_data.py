19229f044fd6a8201bf2671a06f84643556eac7c
Joshua Eliser <jeliser@gmail.com>
2022-02-07 10:56:43 -0600

    master: Added the systems data to a unified dataset.

--- python/wgta_conversion/convert_data.py
+++ python/wgta_conversion/convert_data.py
@@ -12,23 +12,37 @@ import pandas as pd
 from scipy import io
 
 '''
+Save the data frame to the output files.
+'''
+def save_data(opath, name, df):
+  # Create the output directory and write out all the file types
+  os.makedirs(opath, exist_ok=True)
+  fullpath = os.path.join(opath, os.path.splitext(os.path.basename(name))[0])
+  df.to_csv(fullpath + '.csv', index=False)
+  df.to_pickle(fullpath + '.pkl')
+  try:
+    io.savemat(fullpath + '.mat', {f'{os.path.basename(fullpath)}':df.to_dict('list')}, oned_as='column')
+  except:
+    traceback.print_exc()
+
+'''
 Method for processing the files into better data structures
 '''
 def process_file(tup):
   (args, name) = tup
 
   # Scenarios that should be ignored
-  ignore = ['Purge', 'LTF03']
+  ignore = ['Purge', 'LTF03', 'MEG']
   if any(filter(lambda i: i in name, ignore)):
     return
 
-  # Read the data into a pandas data frame and remove all the duplicate lines to only leave the latest updated row.
-  df=pd.read_csv(name, sep=',', low_memory=False)
-  # Automatically remove the system name from the column headers
-  df.columns = [ col.split(':')[-1] for col in df.columns ]
-
   # We only want to process a subset of files 
   if any(filter(lambda n: n in name, ['Systems.csv', 'GNCA.csv'])):
+    # Read the data into a pandas data frame and remove all the duplicate lines to only leave the latest updated row.
+    df = pd.read_csv(name, sep=',', low_memory=False)
+    # Automatically remove the system name from the column headers
+    df.columns = [ col.split(':')[-1] for col in df.columns ]
+
     opath = os.path.join(args.output_dir, os.path.basename(name).split('_')[0])
     print(f'Processing: {name.split(args.input_dir)[-1]} to {opath}')
     
@@ -56,18 +70,64 @@ def process_file(tup):
       if failed:
         return
       # Extract the subset of data from the original data frame using the filtered indexes
-      fdf = df.iloc[idx]
+      save_data(opath, name, df.iloc[idx])
+
+'''
+Method for processing the files into better data structures
+'''
+def combine_file(tup):
+  (args, path) = tup
+
+  # Grab the files we are going to process
+  files = sorted(glob.glob(f'{path}/*.csv'))
+  systems = list(filter(lambda f: 'System' in f, files))[-1]
+  gnc     = list(filter(lambda f: 'GNC' in f, files))[-1]
+
+  # Align data between the Systems file and the GNC file
+  df_systems = pd.read_csv(systems, sep=',', low_memory=False)
+  #df_gnc = pd.read_csv(gnc, sep=',', low_memory=False)
+  # TODO: Align the two files
+
+  # Start building the unified data frame
+  df = pd.DataFrame()
+  headers = ['TIME', 'MET_SUBSECS', 'VEHICLE_STATE', ]
+  pressures = ['ED01_PRESS', 'ED02_PRESS', 'ED03_PRESS', 'EGC_PRESS', 'N2_CTRL_VALVE_FEED_PRESS', 'PROP_TANKS_FEED_PRESS', 'N2_TANKS_PRESS', 'PROP_TANKS_PRESS', 'THRUSTERS_FEED_PRESS']
+  temperatures = ['EGC_THROTTLE_MOTOR_TEMP', 'EA01_FUEL_FEED_TUBE_TEMP', 'ED01_FUEL_FEED_TUBE_TEMP', 'ED01_TEMP', 'ED02_TEMP', 'ED03_TEMP', 'EGC_FUEL_FEED_TUBE_TEMP', 'EGC_TEMP', 'MAIN_FUEL_FEED_TUBE_TEMP', 'N2_TANK1_TEMP', 'N2_TANK2_TEMP', 'PROP_TANK1_BOTTOM_TEMP', 'PROP_TANK1_TOP_TEMP', 'PROP_TANK2_BOTTOM_TEMP', 'PROP_TANK2_TOP_TEMP']
+  currents = ['N2_ISO_VALVE_CURR', 'PROP_ISO_VALVE_CURR', 'PROP_LOAD_VALVE_CURR', 'ED01_CURR', 'ED02_CURR', 'ED03_CURR', 'EA01_CURR', 'EA02_CURR', 'EA03_CURR', 'EA04_CURR', 'EA05_CURR', 'EA06_CURR', 'EA07_CURR', 'EA08_CURR', 'EA09_CURR', 'EA10_CURR', 'EA11_CURR', 'EA12_CURR', 'EGC_THROTTLE_MOTOR_CURR']
+  misc = ['LANDING_LEG_0', 'LANDING_LEG_1', 'LANDING_LEG_2', 'LAND_NOW_ABORT']
+  controls = ['MGC_MASS_ESTIMATE', 'EGC_THROTTLE_POSITION']
+
+  # Load the fields into the data frame
+  for fields in [headers, pressures, temperatures, currents, misc, controls]:
+    for field in fields:
+      df[field] = df_systems[field]
+  
+  print(df)
+
+  opath = os.path.join(args.output_dir, os.path.basename(systems).split('_')[0])
+  name = os.path.basename(opath)
+  save_data(opath, name, df)
+
+  '''
+  # Read the data into a pandas data frame and remove all the duplicate lines to only leave the latest updated row.
+  df=pd.read_csv(name, sep=',', low_memory=False)
+  # Automatically remove the system name from the column headers
+  df.columns = [ col.split(':')[-1] for col in df.columns ]
+
+  # We only want to process a subset of files 
+  if any(filter(lambda n: n in name, ['Systems.csv', 'GNCA.csv'])):
+    opath = os.path.join(args.output_dir, os.path.basename(name).split('_')[0])
+    print(f'Processing: {name.split(args.input_dir)[-1]} to {opath}')
+    
+    # We are going to filter on any SUBSEC column in the CSV.  That's where the data will be aligned for the current timestep
+    keys = list(filter(lambda k: 'SUBSEC' in k, df.columns))
+    if any(keys):
+      if args.verbose:
+        print(f'Filtering on {keys[0]} in {name.split(args.input_dir)[-1]}')
 
       # Create the output directory and write out all the file types
-      os.makedirs(opath, exist_ok=True)
-      fullpath = os.path.join(opath, os.path.splitext(os.path.basename(name))[0])
-      fdf.to_csv(fullpath + '.csv', index=False)
-      fdf.to_pickle(fullpath + '.pkl')
-      try:
-        io.savemat(fullpath + '.mat', {f'{os.path.basename(fullpath)}':fdf.to_dict('list')}, oned_as='column')
-      except:
-        traceback.print_exc()
-
+      save_data(opath, name, df)
+  '''
 
 def main():
   parser = argparse.ArgumentParser(description='Publishes rows of data.')
@@ -90,7 +150,13 @@ def main():
 
   try:
     p = multiprocessing.Pool(args.pool)
+    # Perform the initial processing and cleanup of the raw data files
     p.map(process_file, [(args, filename) for filename in sorted(glob.glob(f'{args.input_dir}/**/*.csv', recursive=True))])
+
+    # Create a single file with the columns and data structures that we need.
+    p.map(combine_file, [(args, path) for path in sorted(glob.glob(f'{args.output_dir}/*'))])
+    
+
   except KeyboardInterrupt: 
     running = False
   except:

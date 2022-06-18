c901412faf292703f5fff6670cb16c547fce9b87
Joshua Eliser <jeliser@gmail.com>
2022-06-18 00:01:10 -0500

    master: Cleaned up the installer script a bit.

--- raspbian/installer.sh
+++ raspbian/installer.sh
@@ -2,13 +2,23 @@
 
 ROOT=$( pwd )
 
-# https://github.com/pololu/drv8835-motor-driver-rpi
-if [[ $( pip freeze | grep wiringpi | wc -l ) -eq 0 ]]; then
-  echo "Installing - Pololu DRV8835 Dual Motor Driver Kit for Raspberry Pi"
-  pip install wiringpi pigpio
+if [[ $( cat /boot/config.txt | grep dtoverlay | wc -l ) -eq 0 ]]; then
+  echo "Setting up the GPIO"
+  echo "dtoverlay=spi0-1cs,cs0_pin=25" | sudo tee -a /boot/config.txt
+fi
 
-  git clone https://github.com/pololu/drv8835-motor-driver-rpi.git /tmp/drv8835-motor-driver-rpi && \
-  cd /tmp/drv8835-motor-driver-rpi && \
+# Installing the base python packages
+pip install -q -r requirements.txt 
+
+# https://github.com/pololu/drv8835-motor-driver-rpi
+if [[ $( pip freeze | grep pololu-drv8835-rpi | wc -l ) -eq 0 ]]; then
+  echo "Installing - Pololu DRV8835 Dual Motor Driver Kit for Raspberry Pi"
+
+  TMP=/tmp/drv8835-motor-driver-rpi
+
+  rm -rf ${TMP} && \
+  git clone https://github.com/pololu/drv8835-motor-driver-rpi.git ${TMP} && \
+  cd ${TMP} && \
   python setup.py install
   cd ${ROOT}
 fi

bb8933b276b125fb45c23c09b240983cd0cb565e
Joshua Eliser <jeliser@gmail.com>
2023-07-12 08:18:22 -0500

    master: Updated the script to include different ways of outputting the results.

--- python/jira_rest_api_query.py
+++ python/jira_rest_api_query.py
@@ -13,18 +13,19 @@ from requests.auth import HTTPBasicAuth
 
 
 def main():
-  parser = argparse.ArgumentParser(description='Publishes rows of data.')
+  parser = argparse.ArgumentParser(description='Parses a text file of all the TODO comments and QUERIES JIRA for the current status.')
   parser.add_argument('-j', '--jira', help='The JIRA URL', type=str, default='jira.atlassian.com')
   parser.add_argument('-p', '--prefix', help='The JIRA ticket prefix', type=str, default='JIRA')
   parser.add_argument('-u', '--user', help='The JIRA username', type=str, required=True)
   parser.add_argument('-t', '--txt', help='A text file with all the tickets in it.  A raw grep\'ed output is sufficient', type=str, required=True)
+  parser.add_argument('-l', '--list', help='Print the output as a list instead of dict', action='store_true', default=False)
   parser.add_argument(      '--verbose-help', help='Prints out the verbose help menu', action='store_true', default=False)
   args = parser.parse_args()
 
   try:
     psword = getpass.getpass()
     step = 50
-    data = []
+    data = dict()
 
     # Find all the tagged todo tickets and build the query
     tickets = sorted(set(re.findall(args.prefix + '-\d{1,6}', open(f'{args.txt}', 'r').read())))
@@ -37,11 +38,20 @@ def main():
         # Query JIRA for the ticket statues
         response = requests.get(query, auth=HTTPBasicAuth(args.user, psword))
         for issue in response.json()['issues']:
-          data.append([issue['key'], issue['fields']['status']['name']])
+          if issue['fields']['status']['name'] in data:
+              data[issue['fields']['status']['name']].append(issue['key'])
+          else:
+              data[issue['fields']['status']['name']] = [issue['key']]
 
     # Sort the listing and print out the results
-    for issue in sorted(data, key=lambda x: (x[1], x[0])):
-      print('{:<12} {:}'.format(*issue))
+    if args.list:
+        for key in sorted(data):
+            for issue in sorted(data[key]):
+                print('{:<12} {:}'.format(issue, key))
+    else:
+        for key in data:
+            print('{:<12} {:}'.format(key, sorted(data[key])))
+        
 
   except KeyboardInterrupt: 
     running = False
